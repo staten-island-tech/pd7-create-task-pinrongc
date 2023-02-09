6a8f17816ed321ebfe9a624da7a312476927b3a1
pinrongc <pinrongc156@gmail.com>
2023-02-09 14:00:30 -0500

    .

--- create-task/main.js
+++ create-task/main.js
@@ -1,5 +1,37 @@
 import './style.css'
 
+/* const cat = "https://catfact.ninja/fact?max_length=140"
+const dog = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
+ */
 
+const fact = "https://official-joke-api.appspot.com/random_joke";
 
+const DOMselectors = {
+button: document.getElementById ("api")
+}
+
+async function getData(url) {
+    try{
+      const response = await fetch(url);
+      if(response.status <200 || response.status >299){
+        console.log(response.status);
+        throw error(response);
+      }else {
+        const data = await response.json();
+
+        document.getElementById("api-response").insertAdjacentHTML("beforeend", `${data.setup} ${data.punchline}`);
+        console.log(data)}
+
+    } catch (error){
+        console.log(error);
+        console.log("sad");
+      } 
+    }
+    
+    getData(fact);
+    
+      DOMselectors.api.addEventListener ("click", function (){
+        getData(fact);
+        })  
+    
 
