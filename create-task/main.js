f1c826d8e95ae3143bb047faeb7d31a48e4673e4
pinrongc <pinrongc156@gmail.com>
2023-02-09 22:20:03 -0500

    .

--- create-task/main.js
+++ create-task/main.js
@@ -1,14 +1,10 @@
 import './style.css'
 
-/* const cat = "https://catfact.ninja/fact?max_length=140"
-const dog = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
- */
+const cat = "https://catfact.ninja/fact?max_length=140"
+//const dog = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
 
-const fact = "https://official-joke-api.appspot.com/random_joke";
+//const fact = "https://official-joke-api.appspot.com/random_joke";
 
-const DOMselectors = {
-button: document.getElementById ("api")
-}
 
 async function getData(url) {
     try{
@@ -19,7 +15,11 @@ async function getData(url) {
       }else {
         const data = await response.json();
 
-        document.getElementById("api-response").insertAdjacentHTML("beforeend", `${data.setup} ${data.punchline}`);
+        document.getElementById("api-response").textContent = data.fact;
+        
+
+       // document.getElementById("api-response").insertAdjacentHTML("beforeend", `${data.setup} ${data.punchline}`);
+       
         console.log(data)}
 
     } catch (error){
@@ -28,10 +28,7 @@ async function getData(url) {
       } 
     }
     
-    getData(fact);
+    getData(cat);
     
-      DOMselectors.api.addEventListener ("click", function (){
-        getData(fact);
-        })  
-    
+  
 
