8f02d279010e329e9791e2c0b1d6d818d2f56368
pinrongc <pinrongc156@gmail.com>
2023-02-24 22:10:49 -0500

    .

--- create-task/main.js
+++ create-task/main.js
@@ -18,7 +18,7 @@ async function getData(url) {
       }else {
         const data = await response.json();
         function createCards(data) {
-      document.getElementById("api-response").insertAdjacentHTML ("beforeend",`<img src="${data}" alt="">`)};
+      document.getElementById("box").insertAdjacentHTML ("beforeend",`<img src="${data}" alt="">`)};
         console.log(data)
         data.forEach((cat) => createCards(cat));
         //document.getElementById("api-response").textContent = data.fact;
@@ -35,29 +35,59 @@ const DOMselectors = {
   cat: document.getElementById ("cat"),
   form: document.getElementById ("form"),
   input: document.getElementById ("input"),
-  changetheme: document.getAnimations("theme"),
+  theme: document.getElementById("theme"),
   history: document.getElementById("history"),
-  remove: document.getElementById("remove")
+  remove: document.getElementById("reset"),
+  display: document.getElementById("apiresponse")
 }
-console.log(DOMselectors);
+console.log(DOMselectors.theme);
   
-DOMselectors.cat.addEventListener ("click", function (){
+/* DOMselectors.cat.addEventListener ("click", function (){
   getData(cat);
-  })  
+  })   */
 
- // const cards = function (menu){ 
-  //}, 
-function remove(){
-  let removebtn = document.getElementById(`remove`)
-  removebtn.addEventListener("click", function(){
-    document.getElementById(`display-card`).remove()
-  })
-}
-remove(); 
 
 DOMselectors.form.addEventListener("submit", function (event){
 let result = DOMselectors.input.value
 const url = `http://shibe.online/api/cats?count=${result}`;
 event.preventDefault();
-getData(url)
+getData(url);
+clear();
 })
+
+function clear(){
+  DOMselectors.input.value= "";
+}
+
+const restart = 
+document.getElementById(`reset`);
+restart.addEventListener("click", () => {
+  document.location.reload();
+})
+
+
+
+DOMselectors.theme.addEventListener("click", function() {
+  if(document.body.classList.contains("dark")){
+  document.body.classList.add("light");
+  document.body.classList.remove("dark");
+  }
+  else{
+   document.body.classList.add("dark");
+   document.body.classList.remove("light");
+ } 
+ }); 
+
+
+/* function remove(){
+  let removebtn = document.getElementById(`remove`)
+  removebtn.addEventListener("click", function(){
+    document.getElementById(`display`).remove()
+  })
+}
+remove();   */
+
+
+
+
+
